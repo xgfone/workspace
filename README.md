18392038295ddaaa2f892b4b0d2932144ba58dbc
xgfone <xgfone@126.com>
2017-10-18 10:27:04 +0800

    Update the script to install all the packages.

--- README.md
+++ README.md
@@ -16,3 +16,17 @@ $ git clone https://github.com/xgfone/workspace  $HOME/.workspace
 $ cd $HOME/.workspace && ./install.sh
 ```
 
+If you want to install all packages, please append the argument `--all` to the install script or use `install-all.sh`. For example,
+
+```shell
+# the command, git, must be installed.
+$ curl -sLf https://github.com/xgfone/workspace/raw/master/install-all.sh | bash
+```
+
+OR
+
+```shell
+$ git clone https://github.com/xgfone/workspace  $HOME/.workspace
+$ cd $HOME/.workspace && ./install.sh --all
+```
+
--- install-all.sh
+++ install-all.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
 
+CWD=$HOME/.workspace
+
+ARGS="--all"
+
+if [ -f $CWD/install.sh ]; then
+    $CWD/install.sh || exit 1
+else
+    curl -sLf https://github.com/xgfone/workspace/raw/master/install.sh | bash
+    if [ $? -ne 0 ]; then
+        echo "Failed to intall all the packages."
+        exit 1
+    fi
+fi
+
+$CWD/install-fcitx.sh $ARGS || exit 1
+$CWD/install-go.sh $ARGS || exit 1
+$CWD/install-yixin.sh $ARGS || exit 1
+$CWD/install-wechat.sh $ARGS || exit 1
+$CWD/install-dbeaver.sh $ARGS || exit 1
+$CWD/install-pip.sh $ARGS || exit 1
+$CWD/install-nerdfonts.sh $ARGS || exit 1
+
