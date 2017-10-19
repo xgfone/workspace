a78edebf7d359caa3bd5bfecab681714f8e9cb46
xgfone <xgfone@126.com>
2017-10-19 11:05:09 +0800

    Add the config of pylint and pep8

--- install-pip.sh
+++ install-pip.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+CWD=$HOME/.workspace
 SUDO=sudo
 INSTALLER=apt-get
 
 $SUDO $INSTALLER install -y python-pip python3-pip
 $SUDO pip install pylint nose pep8 jedi
+
+ln -s $CWD/pylintrc $HOME/.pylintrc
+ln -s $CWD/pep8 $HOME/.config/pep8
 
